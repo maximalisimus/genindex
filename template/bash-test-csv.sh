6f5c22439aeb89f0410f555a97875a6dc4b7aef2
Mikhail Artamonov <maximalis171091@yandex.ru>
2020-12-17 23:02:33 +0300

    Test_bash_csv

---
+++ template/bash-test-csv.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+_csv=$(cat icons2.csv | tr ',' '|' | xargs)
+declare -a _csv_array
+_csv_array=( $_csv )
+unset _csv
+declare -a str
+tmp=$(echo "${_csv_array[0]}" | tr '|' ' ')
+str=( $tmp )
+unset tmp
+echo "${str[0]} ${str[3]} ${str[5]}"
+exit 0
