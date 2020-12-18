4d560f4f6fdc98465715e7b7c014e6ebad2c3631
Mikhail Artamonov <maximalis171091@yandex.ru>
2020-12-18 22:09:45 +0300

    Update

--- template/sh.py
+++ template/sh.py
@@ -1,14 +1,37 @@
 #!/usr/bin/env python3
 
+import os
+import os.path
+import base64
 import csv
+import sys
 
-with open('icons.csv', newline='') as csvfile:
-	spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')	
-	results = []
-	for row in spamreader:
-		results.append(row)
-	for i in range(len(results)):
-		# print(results[i][0], end = '\n')
-		for j in range(len(results[i])):
-			print(results[i][j], end = '\n')
-		# print(' '.join(row))
+csvdata = []
+
+csv_file = "icons.csv"
+
+def csv_reader(file_obj):
+	with open(file_obj, "r") as csvfile:
+		csv_data = csv.reader(csvfile)	
+		for row in csv_data:
+			# print(row) # Debug
+			csvdata.append(row)
+
+def printcsv(listname = csvdata):
+	for i in range(len(listname)):
+		# print(listname[i][0], end = '\n')
+		for j in range(len(listname[i])):
+			print(listname[i][j], end = '\n')
+
+def main():
+	# Debug
+    #if len(sys.argv) <= 1:
+    #    print("No root directory specified!")
+    #    exit(1)
+    # Debug
+    print("Work")
+    # csv_reader(csv_file)
+    # printcsv()
+
+if __name__=="__main__":
+	main()
