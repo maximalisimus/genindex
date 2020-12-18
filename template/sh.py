a097d399db726cd292d61fd64c0f1b1948f39dff
Mikhail Artamonov <maximalis171091@yandex.ru>
2020-12-18 15:06:19 +0300

    Python_Start

--- template/sh.py
+++ template/sh.py
@@ -3,7 +3,12 @@
 import csv
 
 with open('icons.csv', newline='') as csvfile:
-	spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
+	spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')	
+	results = []
 	for row in spamreader:
-		print(row)
+		results.append(row)
+	for i in range(len(results)):
+		# print(results[i][0], end = '\n')
+		for j in range(len(results[i])):
+			print(results[i][j], end = '\n')
 		# print(' '.join(row))
