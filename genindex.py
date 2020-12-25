9c35ca37f4ee6cef81e4d25b4dedc54b64244667
Mikhail Artamonov <maximalis171091@yandex.ru>
2020-12-26 02:09:07 +0300

    python_dirs-files

--- genindex.py
+++ genindex.py
@@ -12,8 +12,7 @@ csvdata: list[Any] = []
 directory = ""
 _font = "sans-serif"
 _bgcolor = "white"
-_exclude_dir: list[Any] = []
-_exclude_file: list[Any] = []
+_exclude: list[Any] = []
 
 csv_file = "config/template/icons.csv"
 icon_path = "config/template/image/"
@@ -22,10 +21,9 @@ def switch(case):
 	return {
 		"-font": 1,
 		"-bgcolor": 2,
-		"-exclude-dir": 3,
-		"-exclude-file": 4,
-		"-v": 5,
-		"-h": 6
+		"-exclude": 3,
+		"-v": 4,
+		"-h": 5
 	}.get(case, None)
 
 def csv_reader(file_obj):
@@ -44,11 +42,26 @@ def compare( value, listname = csvdata):
 			if listname[i][j] == value: _tmp = listname[i][0]
 	return _tmp
 
+def stripCurrentDir(path):
+	return path.replace("./", "").replace("/.", "")
+
 def printcsv(listname = csvdata):
 	for i in range(len(listname)):
 		# print(listname[i][0], end = '\n')
 		for j in range(len(listname[i])):
 			print(listname[i][j], end = '\n')
+
+def Enquiry(lis1):
+	if len(lis1) == 0:
+		return 0
+	else:
+		return 1
+
+def is_part_in_list(str_, words):
+	for word in words:
+		if word.lower() in str_.lower():
+			return True
+	return False
 
 def main():
 	global _font, _bgcolor
@@ -70,21 +83,48 @@ def main():
 		for count in range(len(sys.argv)):
 			if switch(sys.argv[count]) == 1: _font = sys.argv[count+1]
 			if switch(sys.argv[count]) == 2: _bgcolor = sys.argv[count + 1]
-			if switch(sys.argv[count]) == 3: _exclude_dir.append(sys.argv[count + 1])
-			if switch(sys.argv[count]) == 4: _exclude_file.append(sys.argv[count + 1])
-			if switch(sys.argv[count]) == 5: print("Versions")
-			if switch(sys.argv[count]) == 6: print("Help!")
+			if switch(sys.argv[count]) == 3: _exclude.append(sys.argv[count + 1])
+			if switch(sys.argv[count]) == 4: print("Versions")
+			if switch(sys.argv[count]) == 5: print("Help!")
 	# print("Font = " + _font)
 	# print("BGColor = " + _bgcolor)
 	# print("\nExclude dir:")
 	# print(_exclude_dir)
 	# print("\nExclude files: ")
 	# print(_exclude_file)
-	csv_reader(csv_file)
+	# csv_reader(csv_file)
 	# print(csvdata)
 	# printcsv()
 	# _file = compare("rar")
 	# if _file != "": _str = icon_path + _file + ".png"
+
+	for dirpath, dirnames, filenames in os.walk(directory, True, None, False):
+		_real_dir = str(dirpath).replace(directory,"")
+		if Enquiry(_exclude):
+			if not is_part_in_list(_real_dir,_exclude):
+				for a in _exclude:
+					if a in dirnames: dirnames.remove(a)
+				print("Каталог:",_real_dir)
+				if Enquiry(dirnames):
+					dirnames.sort()
+					for dirname in dirnames:
+						print("Direcory:", dirname)
+				else:
+					print("Directory is found!")
+				filenames.sort()
+				for filename in filenames:
+					print("Filename:", filename)
+		else:
+			print("Каталог:", _real_dir)
+			if Enquiry(dirnames):
+				dirnames.sort()
+				for dirname in dirnames:
+					print("Direcory:", dirname)
+			else:
+				print("Directory is found!")
+			filenames.sort()
+			for filename in filenames:
+				print("Filename:", filename)
 
 if __name__=="__main__":
 	main()
