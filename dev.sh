ec39193e09b4f2c9a3f76d13ca879ca8257e52e6
Mikhail Artamonov <maximalis171091@yandex.ru>
2020-12-25 15:55:40 +0300

    rev_windows_test

--- dev.sh
+++ dev.sh
@@ -8,17 +8,17 @@ temp_files=$(mktemp 2>/dev/null) || temp_files=${filesdir}/tmpfile$$
 function show_dir()
 {
 	if [[ -z "$2" ]]; then
-		find "$1" -maxdepth 1 -type d | rev | cut -d '/' -f1 | rev | awk '!/^$/{print $0}' | sort > ${temp_dir}
+		find "$1" -maxdepth 1 -type d | ./modules/reverse.sh | cut -d '/' -f1 | ./modules/reverse.sh | awk '!/^$/{print $0}' | sort > ${temp_dir}
 	else
-		find "$1" -maxdepth 1 -type d | rev | cut -d '/' -f1 | rev | awk '!/^$/{print $0}' | grep -Ev "*${2}*" | sort > ${temp_dir}
+		find "$1" -maxdepth 1 -type d | ./modules/reverse.sh | cut -d '/' -f1 | ./modules/reverse.sh | awk '!/^$/{print $0}' | grep -Ev "*${2}*" | sort > ${temp_dir}
 	fi
 }
 function show_file()
 {
 	if [[ -z "$2" ]]; then
-		find "$1" -maxdepth 1 -type f | rev | cut -d '/' -f1 | rev | awk '!/^$/{print $0}' | sort > ${temp_files}
+		find "$1" -maxdepth 1 -type f | ./modules/reverse.sh | cut -d '/' -f1 | ./modules/reverse.sh | awk '!/^$/{print $0}' | sort > ${temp_files}
 	else
-		find "$1" -maxdepth 1 -type f | rev | cut -d '/' -f1 | rev | awk '!/^$/{print $0}' | grep -Ev "*${2}*" | sort > ${temp_files}
+		find "$1" -maxdepth 1 -type f | ./modules/reverse.sh | cut -d '/' -f1 | ./modules/reverse.sh | awk '!/^$/{print $0}' | grep -Ev "*${2}*" | sort > ${temp_files}
 	fi
 }
 function return_ext()
