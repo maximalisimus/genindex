216ff820f4e5e7240beafcc61fa180ee7829091c
Mikhail Artamonov <maximalis171091@yandex.ru>
2020-12-29 23:21:34 +0300

    python_dir_trees_bicycle

--- test.py
+++ test.py
@@ -8,6 +8,13 @@ import math
 _exclude_dir = []
 _exclude_file = []
 
+def switch(case):
+	return {
+		"-dir": 1,
+		"-exclude-dir": 2,
+		"-exclude-file": 3
+	}.get(case, None)
+
 def Enquiry(lis1):
 	if len(lis1) == 0:
 		return 0
@@ -20,58 +27,50 @@ def is_part_in_list(str_, words):
 			return True
 	return False
 
-def list_files(startpath):
-	count = 0
-	old_count = 0
-	old_list = []
-	old_list.clear()
-	one_list = []
-	one_list.clear()
-	old_subindent = 0
-	for root, dirs, files in os.walk(startpath):
-		_real_dir = str(root).replace(startpath, "")
-		if Enquiry(_exclude_dir):
-			if not is_part_in_list(_real_dir, _exclude_dir):
-				level = root.replace(startpath, '').count(os.sep)
-				indent = ' ' * 2 * (level)
-				subindent = ' ' * 2 * (level + 1)
-				if Enquiry(old_list):
-					old_list.sort()
-					for key in old_list:
-						print('{}{}'.format(old_subindent, key))
-				print('{}{}/'.format(indent, os.path.basename(root)))
-				if count == 0:
-					if Enquiry(_exclude_file):
-						_str_one = set(files)
-						_str_two = set(_exclude_file)
-						for key1 in _str_one:
-							if not is_part_in_list(str(key1),_str_two): one_list.append(key1)
-					else:
-						for f in files:
-							one_list.append(f)
-					count+=1
-					old_count+=1
-					continue
-				if Enquiry(_exclude_file):
-					_str_one = set(files)
-					_str_two = set(_exclude_file)
-					old_list.clear()
-					for key1 in _str_one:
-						if not is_part_in_list(str(key1),_str_two): old_list.append(key1)
-				else:
-					old_list.clear()
-					for f in files:
-						old_list.append(f)
-				old_subindent = subindent
-				count+=1
-	one_list.sort()
-	for f in one_list:
-		print(f)
+def on_trees(startpath,editpath = None):
+	if editpath == None: editpath = startpath
+	basedir = str(editpath).replace(startpath, '')
+	level = editpath.replace(startpath, '').count(os.sep)
+	indent = ' ' * 2 * (level)
+	subindent = ' ' * 2 * (level + 1)
+	dirlist = []
+	filelist = []
+	for key in os.listdir(editpath):
+		paths = os.path.join(editpath,key)
+		if os.path.isdir(paths):
+			if Enquiry(_exclude_dir):
+				if not is_part_in_list(key, _exclude_dir): dirlist.append(key)
+			else: dirlist.append(key)
+		else:
+			if Enquiry(_exclude_file):
+				if not is_part_in_list(key, _exclude_file): filelist.append(key)
+			else:
+				filelist.append(key)
+	if Enquiry(dirlist):
+		dirlist.sort()
+		for key1 in dirlist:
+			paths = os.path.join(editpath,key1)
+			level = paths.replace(startpath, '').count(os.sep)
+			indent = ' ' * 2 * (level)
+			print('{}{}/'.format(indent, key1))
+			on_trees(startpath,paths)
+	if Enquiry(filelist):
+		filelist.sort()
+		for key2 in filelist:
+			print('{}{}'.format(subindent, key2))
 
-_exclude_dir.append(".git")
-_exclude_file.append("gitattributes")
+def main():
+	if len(sys.argv) > 2:
+		for count in range(len(sys.argv)):
+			if switch(sys.argv[count]) == 1: directory = sys.argv[count + 1]
+			if switch(sys.argv[count]) == 2: _exclude_dir.append(sys.argv[count + 1])
+			if switch(sys.argv[count]) == 3: _exclude_file.append(sys.argv[count + 1])
+		if not os.path.isdir(directory):
+			print("Parameter is not the directory", directory)
+			print("Help")
+			exit(1)
+		on_trees(directory)
 
-list_files("../abif-master")
-
-
+if __name__=="__main__":
+	main()
 
